ebea47d6ef58faf6dee235dbe1559bb414b4a251
QuincyMashava <mashavaquincy@gmail.com>
2022-04-20 11:02:41 -0700

    correction of url

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,6 +1,6 @@
 $(function () {
   $('#btn_translate').click(function () {
-    $.get('https://www.fourtonfish.com/hellosalut/hello/' + $('#city_search').val() + '%22)&format=json', function (resp, status) {
+    $.get('https://www.fourtonfish.com/hellosalut/hello/&format=json', function (resp, status) {
       if (status === 'success') {
         $('#hello').text(resp.query.results.hello);
       }
